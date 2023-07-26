ac8cb588df42a5f818d924210371ef8ff4f5c22f
hadiadjd <88900793+liliumorion@users.noreply.github.com>
2023-07-26 17:10:47 +0100

    Completed Assignments

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-function scuberGreetingForFeet(feetNumber){
-  let result
-  if (feetNumber <= 400) {
-    result = "This one is on me!" ;
-  } 
-  else if (feetNumber>400 && feetNumber<=2000){
-    result = "That will be twenty bucks.";
-  } 
-  else if (feetNumber > 2000 && feetNumber <=2500){
-    result = "I will gladly take your thirty bucks."; 
-  } 
-  else if (feetNumber > 2500){
-    result = "No can do.";
-  }
-  return result;
-  } 
-
-
-function ternaryCheckCity(destination){
- 
-
-  return destination === "NYC"? "Ok, sounds good." : "No go.";
-  
+function scuberGreetingForFeet(feetNumber) {
+    let result
+    if (feetNumber <= 400) {
+        result = "This one is on me!";
+    } else if (feetNumber > 400 && feetNumber <= 2000) {
+        result = "That will be twenty bucks.";
+    } else if (feetNumber > 2000 && feetNumber <= 2500) {
+        result = "I will gladly take your thirty bucks.";
+    } else if (feetNumber > 2500) {
+        result = "No can do.";
+    }
+    return result;
 }
 
 
-function switchOnCharmFromTip(tip){
-  
-  switch(tip){
-  case 'generous':
-    return'Thank you so much.';
-    
-  case 'not as generous':
-   return'Thank you.';
-    
-  default:
-    return'Bye.';
-  }
-  
+function ternaryCheckCity(destination) {
+
+
+    return destination === "NYC" ? "Ok, sounds good." : "No go.";
+
+}
+
+
+function switchOnCharmFromTip(tip) {
+
+    switch (tip) {
+        case 'generous':
+            return 'Thank you so much.';
+
+        case 'not as generous':
+            return 'Thank you.';
+
+        default:
+            return 'Bye.';
+    }
+
 }
