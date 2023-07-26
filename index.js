7f5956ca4d0b59a35f53cdae6807fc5c0123c2ba
hadiadjd <celestialgeek21@gmail.com>
2023-07-26 17:06:38 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetNumber){
+  let result
+  if (feetNumber <= 400) {
+    result = "This one is on me!" ;
+  } 
+  else if (feetNumber>400 && feetNumber<=2000){
+    result = "That will be twenty bucks.";
+  } 
+  else if (feetNumber > 2000 && feetNumber <=2500){
+    result = "I will gladly take your thirty bucks."; 
+  } 
+  else if (feetNumber > 2500){
+    result = "No can do.";
+  }
+  return result;
+  } 
+
+
+function ternaryCheckCity(destination){
+ 
+
+  return destination === "NYC"? "Ok, sounds good." : "No go.";
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  
+  switch(tip){
+  case 'generous':
+    return'Thank you so much.';
+    
+  case 'not as generous':
+   return'Thank you.';
+    
+  default:
+    return'Bye.';
+  }
+  
 }
